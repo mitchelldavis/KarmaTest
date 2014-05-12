4a764570864ae3ff951387ac4c00c175996cbe4b
mitchell.davis <mitchell.davis@veriship.com>
2014-05-12 08:52:42 -0500

    Initial Commit.

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+A simple example ap of how Karma JS works.
---
+++ Test.js
@@ -0,0 +1,8 @@
+describe("A suite", function() {
+    it("Some Random Test", function() {
+        expect(true).toBe(true);
+    });
+    it("Failing Test", function() {
+        expect(true).toBe(false);
+    });
+});
