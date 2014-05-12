58676d6f36f5b9c6e0231b55fdf7bc60e12977db
mitchell.davis <mitchell.davis@veriship.com>
2014-05-12 09:13:45 -0500

    Added some msbuild scripts.

--- Test.js
+++ Test.js
@@ -2,7 +2,4 @@ describe("A suite", function() {
     it("Some Random Test", function() {
         expect(true).toBe(true);
     });
-    it("Failing Test", function() {
-        expect(true).toBe(false);
-    });
 });
